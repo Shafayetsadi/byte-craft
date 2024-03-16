b90e66b9ae4d5662a541ca520ef1a1dda29b3ec3
Shafayet Sadi <shafayet.sadi@gmail.com>
2024-03-16 18:59:06 +0600

    Day-01 LinkedList in C

--- data-structures/linked_list.c
+++ data-structures/linked_list.c
@@ -3,60 +3,64 @@
 #include <stdlib.h>
 #include <strings.h>
 
-// TODO: Fix the deleteData() function
-
-struct Node {
+typedef struct Node {
   void *data;
   struct Node *next;
   struct Node *prev;
-};
+} Node;
 
-struct Node *newNode(void *data);
-void insertBegin(struct Node **head, void *data);
-void insertEnd(struct Node **head, void *data);
-void insertIndex(struct Node **head, void *data, int index);
-void *deleteBegin(struct Node **head);
-void *deleteEnd(struct Node **head);
-void *deleteData(struct Node **head, void *data);
-void printList(struct Node *head, void (*printData)(void *));
+Node *newNode(void *data);
+void insertBegin(Node **head, void *data);
+void insertEnd(Node **head, void *data);
+void insertIndex(Node **head, void *data, size_t index);
+void *deleteBegin(Node **head);
+void *deleteEnd(Node **head);
+void *deleteIndex(Node **head, size_t index);
+void printList(Node *head, void (*printData)(void *));
+void freeList(Node *head);
 
 void printInt(void *data) { printf("%d ", *((int *)data)); }
-
 void printFloat(void *data) { printf("%f ", *((float *)data)); }
-
 void printString(void *data) { printf("%s ", (char *)data); }
 
 int main() {
-  int *a = malloc(sizeof(int));
-  *a = 5;
-  // printf("%d\n", *(int *)a);
   printf("Hello World!\n");
 
   int x = 10, y = 15, z = 20;
-  struct Node *head = newNode(&x);
+  Node *head = newNode(&x);
   insertBegin(&head, &y);
   insertBegin(&head, &z);
+  printList(head, printInt);
+
+  int *a = malloc(sizeof(int));
+  *a = 5;
+  // printf("%d\n", *(int *)a);
+  insertIndex(&head, a, 3);
+  printList(head, printInt);
+  insertIndex(&head, a, 5);
+  printList(head, printInt);
+
   insertEnd(&head, &x);
   insertEnd(&head, &y);
   insertEnd(&head, &z);
   printList(head, printInt);
-  insertIndex(&head, a, 3);
-  printList(head, printInt);
+
+  printf("Delete:\n");
   deleteBegin(&head);
   printList(head, printInt);
   deleteEnd(&head);
   printList(head, printInt);
-  int *data = malloc(sizeof(int));
-  *data = 15;
-  deleteData(&head, data);
+  deleteIndex(&head, 2);
   printList(head, printInt);
+
+  freeList(head);
 }
 
-struct Node *newNode(void *data) {
-  struct Node *node = (struct Node *)malloc(sizeof(struct Node));
+Node *newNode(void *data) {
+  Node *node = (Node *)malloc(sizeof(Node));
   if (node == NULL) {
     printf("Memory allocation failed\n");
-    exit(1);
+    exit(EXIT_FAILURE);
   }
   node->data = data;
   node->next = NULL;
@@ -64,85 +68,135 @@ struct Node *newNode(void *data) {
   return node;
 }
 
-void insertBegin(struct Node **head, void *data) {
-  struct Node *node = newNode(data);
-  (*head)->prev = node;
-  node->next = *head;
+void insertBegin(Node **head, void *data) {
+  Node *node = newNode(data);
+  if (*head not_eq NULL) {
+    (*head)->prev = node;
+    node->next = *head;
+  }
   *head = node;
 }
 
-void insertEnd(struct Node **head, void *data) {
-  struct Node *curr = *head;
-  while (curr->next != NULL) {
+void insertEnd(Node **head, void *data) {
+  Node *curr = *head;
+  if (curr == NULL) {
+    insertBegin(head, data);
+    return;
+  }
+  while (curr->next not_eq NULL) {
     curr = curr->next;
   }
-  struct Node *node = newNode(data);
+  Node *node = newNode(data);
   node->prev = curr;
   curr->next = node;
 }
 
-void insertIndex(struct Node **head, void *data, int index) {
-  struct Node *curr = *head;
-  int i = 1;
+void insertIndex(Node **head, void *data, size_t index) {
+  if (index == 0) {
+    insertBegin(head, data);
+    return;
+  }
+  Node *curr = *head;
+  size_t i = 1;
   while (curr not_eq NULL and i < index) {
     curr = curr->next;
     i++;
   }
   if (curr == NULL) {
-    printf("Out of index bound!\n");
+    printf("Index out of bound\n");
     return;
   }
-  struct Node *node = newNode(data);
-  struct Node *temp = curr->next;
-  curr->next = node;
-  node->prev = curr;
-
-  node->next = temp;
-  temp->prev = node;
-}
-
-void *deleteBegin(struct Node **head) {
-  void *temp = (*head)->data;
-  *head = (*head)->next;
-  return temp;
-}
-
-void *deleteEnd(struct Node **head) {
-  struct Node *curr = *head;
-  while (curr->next->next not_eq NULL) {
-    curr = curr->next;
-  }
-  void *temp = curr->next->data;
-  curr->next = NULL;
-  return temp;
-}
-
-void *deleteData(struct Node **head, void *data) {
-  struct Node *curr = *head;
-  while (curr->next not_eq NULL) {
-    if (curr->data == data) {
-      break;
-    }
-    curr = curr->next;
-  }
-  if (curr->data == data) {
-    struct Node *prev = curr->prev;
-    struct Node *next = curr->next;
-    void *temp = curr->data;
-    prev->next = next;
-    next->prev = prev;
-    return temp;
+  Node *node = newNode(data);
+  if (curr->next == NULL) {
+    curr->next = node;
+    node->prev = curr;
   } else {
-    printf("Data not found.\n");
+    node->next = curr->next;
+    curr->next->prev = node;
+    curr->next = node;
+    node->prev = curr;
+  }
+}
+
+void *deleteBegin(Node **head) {
+  if (*head == NULL) {
+    printf("List is empty\n");
     return NULL;
   }
+  Node *temp = *head;
+  *head = (*head)->next;
+  if (*head not_eq NULL) {
+    (*head)->prev = NULL;
+  }
+  void *data = temp->data;
+  free(temp);
+  return data;
 }
 
-void printList(struct Node *head, void (*printData)(void *)) {
-  struct Node *curr = head;
+void *deleteEnd(Node **head) {
+  if (*head == NULL) {
+    printf("List is empty\n");
+    return NULL;
+  }
+  Node *curr = *head;
+  while (curr->next not_eq NULL) {
+    curr = curr->next;
+  }
+  if (curr->prev not_eq NULL) {
+    curr->prev->next = NULL;
+  } else {
+    *head = NULL;
+  }
+  void *data = curr->data;
+  free(curr);
+  return data;
+}
+
+void *deleteIndex(Node **head, size_t index) {
+  if (*head == NULL) {
+    printf("List is empty\n");
+    return NULL;
+  }
+  if (index == 0) {
+    void *data = (*head)->data;
+    deleteBegin(head);
+    return data;
+  }
+  Node *curr = *head;
+  size_t i = 0;
+  while (curr not_eq NULL and i < index) {
+    curr = curr->next;
+    i++;
+  }
+  if (curr == NULL) {
+    printf("Index out of bound\n");
+    return NULL;
+  }
+  void *data = curr->data;
+  if (curr->prev not_eq NULL) {
+    curr->prev->next = curr->next;
+  }
+  if (curr->next not_eq NULL) {
+    curr->next->prev = curr->prev;
+  }
+  free(curr);
+  return data;
+}
+
+void printList(Node *head, void (*printData)(void *)) {
+  Node *curr = head;
   while (curr not_eq NULL) {
     printData(curr->data);
     curr = curr->next;
   }
   printf("\n");
+}
+
+void freeList(Node *head) {
+  while (head not_eq NULL) {
+    Node *temp = head;
+    head = head->next;
+    temp->data = NULL;
+    free(temp);
+  }
 }
